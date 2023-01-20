b81febde00288667dde6fe71ce95364e654fddd9
KipaTa <kirsi.tainio@gmail.com>
2023-01-20 15:10:50 +0200

    Feat: Context local storage & user context &redirect

--- src/Components/Startup/StartupForm.js
+++ src/Components/Startup/StartupForm.js
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { loginUser } from "../../api/userInfo";
+import { storageSave } from "../../utils/storage";
+import { useNavigate } from 'react-router-dom'
+import { useUser } from "../../context/UserContext";
 
 const usernameConfig = {
   required: true,
@@ -8,27 +11,41 @@ const usernameConfig = {
 };
 
 const StartupForm = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  // Hooks
+  const { register, handleSubmit, formState: { errors } } = useForm()
+  const { user, setUser } = useUser()
+  const navigate = useNavigate()
 
+  //Local state
   const [ loading, setLoading ] = useState(false)
+  const [ apiError, setApiError ] = useState(null)
 
+  //Side Effects
+  useEffect(() => {
+    if (user !== null){
+      navigate('/profile')
+    }
+  }, [ user, navigate])
+
+ // Event Handlers
   const onSubmit = async ({username}) => {
     setLoading(true)
-    const [error, user] = await loginUser(username);
-    console.log("Error: ", error)
-    console.log("User: ", user)
+    const [error, userResponse] = await loginUser(username)
+    if (error !== null) {
+      setApiError(error)
+    }
+    if (userResponse !== null){
+      storageSave('translation-user', userResponse)
+      setUser(userResponse)
+    }
     setLoading(false)
   };
 
-  console.log(errors);
 
+  // Render Functions
   const errorMessage = (() => {
     if (!errors.username) {
-      return null;
+      return null
     }
     if (errors.username && errors.username.type === "required") {
       return <span>Username is required</span>;
@@ -55,6 +72,7 @@ const StartupForm = () => {
         <button type="submit" disabled={ loading }>Continue</button>
 
         { loading && <p>Logging in...</p> }
+        { apiError && <p>{ apiError }</p>}
       </form>
     </>
   );
