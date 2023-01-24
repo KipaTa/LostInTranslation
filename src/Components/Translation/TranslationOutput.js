2637cd239b4ead5902e077c88c56d4cd764d0d2d
Adalmiina <adalmiinasuikkanen3@gmail.com>
2023-01-24 13:16:58 +0200

    Feat: showing the photos when the user submits an input

--- src/Components/Translation/TranslationOutput.js
+++ src/Components/Translation/TranslationOutput.js
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import { addTranslation } from "../../api/translation";
 import { STORAGE_KEY_USER } from "../../const/storageKeys";
 import { useUser } from "../../context/UserContext";
 import { storageSave } from "../../utils/storage";
 import TranslationInput from "./TranslationInput";
+import TranslationItem from "./TranslationItem";
 
 const TranslationOutput = () => {
     const {user, setUser} = useUser()
+		const [letters, setLetters] = useState([])
 
   const handleClick = async (notes) => {
 		const [error, updatedUser] = await addTranslation(user, notes)
@@ -16,18 +19,19 @@ const TranslationOutput = () => {
 		storageSave(STORAGE_KEY_USER,updatedUser)
 		setUser(updatedUser)
 
+		const array = notes.toLowerCase().match(/[a-z]/g)
+		setLetters(array)
+
 		console.log(error)
 		console.log("Result" , updatedUser)
 
   };
-
-  //const array = note.toString().split("");
-  //const TranslationList = array.map((letter, index) => (<TranslationItem key={index + "-" + letter} src={`individial_signs/${letter}.png`}></TranslationItem>));
-   
+	
   return (
     <>
       <h1>Translation Output</h1>
-      <TranslationInput onClick={handleClick}></TranslationInput>
+      <TranslationInput onClick={handleClick} ></TranslationInput>
+			<TranslationItem array={letters}/>
     </>
   );
 };
