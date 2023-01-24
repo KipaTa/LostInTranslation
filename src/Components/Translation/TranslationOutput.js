6d797d9d54d48fbbf379ab091dced7640f628391
KipaTa <kirsi.tainio@gmail.com>
2023-01-24 13:56:47 +0200

    Fix: Translation fixes, not finished

--- src/Components/Translation/TranslationOutput.js
+++ src/Components/Translation/TranslationOutput.js
@@ -1,18 +1,37 @@
+import { useState } from "react";
+import { addTranslation } from "../../api/translation";
+import { STORAGE_KEY_USER } from "../../const/storageKeys";
+import { useUser } from "../../context/UserContext";
+import { storageSave } from "../../utils/storage";
+import TranslationInput from "./TranslationInput";
 
-const TranslationOutput = (input) => {
-  const array = input.toString().split("");
-  console.log(array);
-  const translationList = array.map((letter, index) => (
-    <img
-      key={index + "-" + letter}
-      alt=""
-      src={`individial_signs/${letter}.png`}
-    ></img>
-  ));
+
+const TranslationOutput = () => {
+    const {user, setUser} = useUser()
+		const [letters, setLetters] = useState([])
+
+  const handleClick = async (notes) => {
+		const [error, updatedUser] = await addTranslation(user, notes)
+		if(error !== null ) {
+			return 
+		}
+
+		storageSave(STORAGE_KEY_USER,updatedUser)
+		setUser(updatedUser)
+
+		const array = notes.toLowerCase().match(/[a-z]/g)
+		setLetters(array)
+
+		console.log(error)
+		console.log("Result" , updatedUser)
+
+  };
+	
   return (
     <>
       <h1>Translation Output</h1>
-      {translationList}
+      <TranslationInput onClick={handleClick} ></TranslationInput>
+			
     </>
   );
 };
