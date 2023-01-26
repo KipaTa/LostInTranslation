b7d740b8897029266f9407e01fe242dd8c4ece0d
KipaTa <kirsi.tainio@gmail.com>
2023-01-26 15:18:02 +0200

    Feat: Comments to the clarify the code

--- src/hoc/withAuth.js
+++ src/hoc/withAuth.js
@@ -1,6 +1,9 @@
 import { Navigate } from "react-router-dom"
 import { useUser } from "../context/UserContext"
 
+
+//withAuth checks if user has signed in. If signed, user is able to navigate forvard. 
+//If not signed in or user logs out, user is returned to Startup page. 
 const withAuth = Component => props => {
     const { user } = useUser()
     if (user !== null) {
